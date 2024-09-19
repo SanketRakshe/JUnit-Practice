674e3264a4f8f653f905b7b4b8c3a66462506ad1
SanketRakshe <sanketrakshe07@gmail.com>
2024-09-19 16:11:29 +0530

    FIX: some bugs fixed

--- Testing/src/main/java/com/example/calculator/CalculatorController.java
+++ Testing/src/main/java/com/example/calculator/CalculatorController.java
@@ -1,4 +1,4 @@
-package conm.example.calculator;
+package com.example.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
